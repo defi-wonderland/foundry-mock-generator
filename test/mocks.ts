317d8d52aeea34cbf304dde1010d1b0b1b4b578b
Gas <86567384+gas1cent@users.noreply.github.com>
2024-04-04 18:01:56 +0400

    feat: using `solc-typed-ast` (#47)
    Co-authored-by: AgusDuha <81362284+agusduha@users.noreply.github.com>

---
+++ test/mocks.ts
@@ -0,0 +1,52 @@
+import {
+  FunctionDefinition,
+  VariableDeclaration,
+  ParameterList,
+  ContractDefinition,
+  ImportDirective,
+  SymbolAlias,
+  Identifier,
+  TypeName,
+  ArrayTypeName,
+  Mapping,
+} from 'solc-typed-ast';
+
+export function mockFunctionDefinition(mockFunctionDefinition: Partial<FunctionDefinition>): FunctionDefinition {
+  return mockFunctionDefinition as FunctionDefinition;
+}
+
+export function mockParameterList(mockParameterList: Partial<ParameterList>): ParameterList {
+  return mockParameterList as ParameterList;
+}
+
+export function mockVariableDeclaration(mockVariableDeclaration: Partial<VariableDeclaration>): VariableDeclaration {
+  return mockVariableDeclaration as VariableDeclaration;
+}
+
+export function mockContractDefinition(mockContractDefinition: Partial<ContractDefinition>): ContractDefinition {
+  return mockContractDefinition as ContractDefinition;
+}
+
+export function mockImportDirective(mockImportDirective: Partial<ImportDirective>): ImportDirective {
+  return mockImportDirective as ImportDirective;
+}
+
+export function mockSymbolAliases(mockSymbolAliases: Partial<SymbolAlias[]>): SymbolAlias[] {
+  return mockSymbolAliases as SymbolAlias[];
+}
+
+export function mockIdentifier(mockIdentifier: Partial<Identifier>): Identifier {
+  return mockIdentifier as Identifier;
+}
+
+export function mockArrayTypeName(mockArrayTypeName: Partial<ArrayTypeName>): ArrayTypeName {
+  return mockArrayTypeName as ArrayTypeName;
+}
+
+export function mockTypeName(mockTypeName: Partial<TypeName>): TypeName {
+  return mockTypeName as TypeName;
+}
+
+export function mockMapping(mockMapping: Partial<Mapping>): Mapping {
+  return mockMapping as Mapping;
+}

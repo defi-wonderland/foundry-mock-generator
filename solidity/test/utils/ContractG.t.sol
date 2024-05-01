8d63235ff1e9b6a8fdad34c96a9df86ac9e8bce5
AgusDuha <81362284+agusduha@users.noreply.github.com>
2024-05-01 08:30:49 -0300

    feat: add call to internal state variables in mocks (#76)

--- solidity/test/utils/ContractG.t.sol
+++ solidity/test/utils/ContractG.t.sol
@@ -227,4 +227,126 @@ contract UnitMockContractG is Test, SmockHelper {
     (_value) = _contractTest.twoDimensionalStringArray(1, 1);
     assertEq(_value, '40');
   }
+
+  function test_Set_NestedStruct() public {
+    ContractG.CommonStruct memory _commonStruct = ContractG.CommonStruct(20);
+    ContractG.NestedStruct memory _nestedStruct = ContractG.NestedStruct(10, _commonStruct);
+
+    _contractTest.set_nestedStruct(_nestedStruct);
+
+    (uint256 _counter, ContractG.CommonStruct memory _commonResult) = _contractTest.nestedStruct();
+
+    assertEq(_counter, 10);
+    assertEq(_commonResult._value, 20);
+  }
+
+  function test_Call_NestedStruct() public {
+    ContractG.CommonStruct memory _commonStruct = ContractG.CommonStruct(20);
+    ContractG.NestedStruct memory _nestedStruct = ContractG.NestedStruct(10, _commonStruct);
+
+    _contractTest.mock_call_nestedStruct(_nestedStruct);
+
+    (uint256 _counter, ContractG.CommonStruct memory _commonResult) = _contractTest.nestedStruct();
+
+    assertEq(_counter, 10);
+    assertEq(_commonResult._value, 20);
+  }
+
+  function test_Call_SetNestedStruct() public {
+    ContractG.CommonStruct memory _commonStruct = ContractG.CommonStruct(20);
+    ContractG.NestedStruct memory _nestedStruct = ContractG.NestedStruct(10, _commonStruct);
+
+    _contractTest.mock_call_setNestedStruct(_nestedStruct);
+    _contractTest.setNestedStruct(_nestedStruct);
+
+    (uint256 _counter, ContractG.CommonStruct memory _commonResult) = _contractTest.nestedStruct();
+
+    assertEq(_counter, 10);
+    assertEq(_commonResult._value, 20);
+  }
+
+  function test_Set_StructArray() public {
+    ContractG.CommonStruct[] memory _structArray = new ContractG.CommonStruct[](2);
+    _structArray[0] = ContractG.CommonStruct(10);
+    _structArray[1] = ContractG.CommonStruct(20);
+
+    _contractTest.set_structArray(_structArray);
+    (uint256 _value) = _contractTest.structArray(0);
+    assertEq(_value, 10);
+
+    (_value) = _contractTest.structArray(1);
+    assertEq(_value, 20);
+  }
+
+  function test_Call_StructArray() public {
+    ContractG.CommonStruct[] memory _structArray = new ContractG.CommonStruct[](2);
+    _structArray[0] = ContractG.CommonStruct(10);
+    _structArray[1] = ContractG.CommonStruct(20);
+
+    _contractTest.mock_call_structArray(0, _structArray[0]);
+    (uint256 _value) = _contractTest.structArray(0);
+    assertEq(_value, 10);
+
+    _contractTest.mock_call_structArray(1, _structArray[1]);
+    (_value) = _contractTest.structArray(1);
+    assertEq(_value, 20);
+  }
+
+  function test_Set_StructArrayInternal() public {
+    ContractG.CommonStruct[] memory _structArray = new ContractG.CommonStruct[](2);
+    _structArray[0] = ContractG.CommonStruct(10);
+    _structArray[1] = ContractG.CommonStruct(20);
+
+    _contractTest.set__structArrayInternal(_structArray);
+
+    ContractG.CommonStruct[] memory _resultArray = _contractTest.call__structArrayInternal();
+    assertEq(_resultArray[0]._value, 10);
+    assertEq(_resultArray[1]._value, 20);
+  }
+
+  function test_Set_Finished() public {
+    bytes32 _key = 'key';
+
+    _contractTest.set_finished(_key, true);
+
+    bool _value = _contractTest.finished(_key);
+
+    assertTrue(_value);
+  }
+
+  function test_Call_Finished() public {
+    bytes32 _key = 'key';
+    _contractTest.mock_call_finished(_key, true);
+
+    bool _value = _contractTest.finished(_key);
+    assertTrue(_value);
+  }
+
+  function test_Set_FinishedInternal() public {
+    bytes32 _key = 'key';
+    _contractTest.set__finishedInternal(_key, true);
+
+    bool _value = _contractTest.call__finishedInternal(_key);
+    assertTrue(_value);
+  }
+
+  function test_Set_DoubleFinishedInternal() public {
+    bytes32 _key0 = 'key0';
+    bytes32 _key1 = 'key1';
+    _contractTest.set__doubleFinishedInternal(_key0, _key1, true);
+
+    bool _value = _contractTest.call__doubleFinishedInternal(_key0, _key1);
+    assertTrue(_value);
+  }
+
+  function test_Set_NestedStructsInternal() public {
+    bytes32 _key = 'key';
+    ContractG.NestedStruct memory _nestedStruct = ContractG.NestedStruct(10, ContractG.CommonStruct(20));
+
+    _contractTest.set__nestedStructsInternal(_key, _nestedStruct);
+
+    ContractG.NestedStruct memory _result = _contractTest.call__nestedStructsInternal(_key);
+    assertEq(_result._counter, 10);
+    assertEq(_result._common._value, 20);
+  }
 }
